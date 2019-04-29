eb83777076b970cb3111f74a7e38057c1b746ba5
335880 <44164529+335880@users.noreply.github.com>
2019-04-29 14:01:17 +0100

    This is the GUI

--- gui/src/ofApp.cpp
+++ gui/src/ofApp.cpp
@@ -2,70 +2,236 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ofBackground(118, 196, 242);
+	myFont.load("Robotronica", 16);
+	
+	
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
+
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	
+
+
+	ofColor blue;
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("TWITTER APPLICATION 2019", 300, 300);
+
+	ofDrawRectangle(10, 10, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(20, 20, 180, 80);
+	
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+	
+	ofSetColor(blue);
+
+
+	ofDrawRectangle(10, 130, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(20, 140, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(10, 250, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(20, 260, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(10, 370, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(20, 380, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(10, 490, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(20, 500, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(810, 10, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(820, 20, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+
+	ofDrawRectangle(810, 130, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(820, 140, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(810, 250, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(820, 260, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(810, 370, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(820, 380, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+	ofDrawRectangle(810, 490, 200, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(820, 500, 180, 80);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+
+	ofSetColor(blue);
+
+
+
+	ofDrawRectangle(120, 650, 800, 100);
+
+	blue.r = 31;
+	blue.g = 54;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawRectangle(260, 120, 500, 500);
+
+	blue.r = 82;
+	blue.g = 100;
+	blue.b = 255;
+	ofSetColor(blue);
+
+	ofDrawCircle(50, 680, 45);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Count Total Tweets", 25, 75);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Count Tweets-Politics", 25, 190);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Count Tweets-Money", 25, 310);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Print Tweets-Uber", 825, 310);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Print Tweets-Paris", 825, 190);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Print Tweets-DreamWorks", 825, 75);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Custom 1", 25, 420);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Custom 2", 25, 530);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Custom 3", 825, 420);
+
+	ofSetColor(120, 255, 100);
+	ofDrawBitmapString("Custom 4", 825, 530);
+
+	ofSetColor(255, 0, 0);
+	ofDrawBitmapString("EXIT", 27, 680);
+
+
+
+	
+
+	
+
 
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
